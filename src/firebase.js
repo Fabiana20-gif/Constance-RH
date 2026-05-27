6e2ae6eb4c2b0b8cf6e3b0d07c93455f4724b97d
Fabiana20-gif <fa_bg@hotmail.com>
2026-05-27 17:51:20 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
+ 
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey:            "AIzaSyCbbd16pLkSH7UPCE6z04-XHJLBz7i76dI",
+  authDomain:        "constance-rh.firebaseapp.com",
+  projectId:         "constance-rh",
+  storageBucket:     "constance-rh.firebasestorage.app",
+  messagingSenderId: "394071965825",
+  appId:             "1:394071965825:web:540f99173a69ef53da8d80",
 };
-
+ 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+ 
